cb3342896d3443df8d18e83aaf353ff5a9c460c6
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-08-27 13:59:23 +0500

    file upload module complete

--- routes/songs.js
+++ routes/songs.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser')
 const songRouter = express.Router();
 songRouter.use(bodyParser.json());
 const multer = require('multer')
-
+const fileUpload =require('express-fileupload');
+const path =require('path');
+songRouter.use(fileUpload({ safeFileNames: true, preserveExtension: true }));
 
 // const multerStorage = multer.diskStorage({
 //     destination:(req,file,cb) => {
@@ -29,22 +31,32 @@ songRouter.route('/')
     
 })
 .post((req,res,next) => {
-    Song.create(req.body,(err,resp) =>{
-      if(req.file) {
-          console.log(req.file);
-          var file = req.file;
-          var filename = file.name
+    // Song.create(req.body,(err,resp) =>{
+      if(req.files) {
+          console.log(req.files);
+          var file = req.files.file;
+          var filename = file.name;
           console.log(filename);
-          res.statusCode = 200
-          res.setHeader('Content_Type','text/plain');
-          // res.json(req.file);
-          res.end('Successfully inserted audio file');    
+          file.mv(path.resolve(__dirname,'../public/images',filename),(err) => {
+              if(err){
+                  console.log(err);
+                return;
+              }
+              else{
+                res.statusCode = 200;
+                res.end('File uploaded !');
+              }
+          })
+          
       }
-      res.statusCode = 404
-      res.setHeader('Content_Type','text/plain');
-      res.end('Not Successfully inserted audio file');
+      else{
+        res.statusCode = 404
+        res.setHeader('Content_Type','text/plain');
+        res.end('Not Successfully inserted audio file');
+      }
       
-    })
+      
+
     
     
 });
